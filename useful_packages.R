804224c38ba0031014d560fcf343ec5b3f61ae1b
James Mba Azam <jamesazam@sun.ac.za>
2019-07-24 18:34:48 +0200

    added a script for collating a list of r packages I think I'll find useful

---
+++ useful_packages.R
@@ -0,0 +1,5 @@
+library(latex2exp) #package for rendering expressions with latex code
+help(package = 'latex2exp') #documentation
+latex2exp_examples()
+# only function is TeX('latex expression in math environment and two backslashes\\') e.g
+?latex2exp::TeX
