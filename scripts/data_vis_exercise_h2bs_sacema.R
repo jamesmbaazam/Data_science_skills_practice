59f15c50ebcedb1d60081c55bc9b4fd5054e9327
James Mba Azam <jamesazam@sun.ac.za>
2019-07-25 13:13:36 +0200

    added a script for the how to be a scientist assignment on data visualisation

---
+++ scripts/data_vis_exercise_h2bs_sacema.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+
+#data
+library(gapminder)
+library(datasets)
+help(package = 'datasets')
+help(package = 'gapminder')
+
+
+gapminder <- gapminder
+head(gapminder)
