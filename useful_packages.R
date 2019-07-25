c68be99f5b673e70d4b05c6863a9aa099eece4c6
James Mba Azam <jamesazam@sun.ac.za>
2019-07-25 13:13:03 +0200

    added the leaflet package to the list

--- useful_packages.R
+++ useful_packages.R
@@ -3,3 +3,8 @@ help(package = 'latex2exp') #documentation
 latex2exp_examples()
 # only function is TeX('latex expression in math environment and two backslashes\\') e.g
 ?latex2exp::TeX
+
+
+install.packages('leaflet')
+library('leaflet')
+help(package = 'leaflet') #documentation
