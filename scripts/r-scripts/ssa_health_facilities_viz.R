7aa27d604f418315374e196ae2fa6c395ec33db1
James Mba Azam <jamesazam@sun.ac.za>
2019-10-11 14:56:26 +0200

    added a title and flipped the axes for better reading

--- scripts/r-scripts/ssa_health_facilities_viz.R
+++ scripts/r-scripts/ssa_health_facilities_viz.R
@@ -29,8 +29,13 @@ percentage_sources$ll_source <- forcats::fct_explicit_na(percentage_sources$ll_s
 
 percentage_sources
 
-ggplot(data = percentage_sources, aes(x = reorder(ll_source, percentage), y = percentage), fill = 'variable') + 
+data_sources_plot <- ggplot(data = percentage_sources, aes(x = reorder(ll_source, percentage), y = percentage), fill = 'variable') + 
     geom_bar(stat = 'identity') +
-    labs(x = 'Source', y = 'Percent of facilities') +
+    labs(x = 'Source', y = 'Percent of facilities', title = paste('Sources of data on sub-Saharan Health facilities', '\n', '(percentage of total)')) +
+    coord_flip() +
     theme_economist() +
-    theme(axis.title.x = element_text(size = 14), axis.title.y = element_text(size = 14), axis.text.x = element_text(angle = 90, hjust = 1)) 
+    theme(axis.title.x = element_text(size = 14), axis.title.y = element_text(size = 14), plot.title = element_text(hjust = 0.5)) 
+
+data_sources_plot
+
+ggsave(filename = 'figures/ssa_health_facilities_data_souces.png', plot = data_sources_plot, width = 7.5, units = 'in')
